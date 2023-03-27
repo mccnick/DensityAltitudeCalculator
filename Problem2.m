2cf2f086de4bfeeea5f7ee35d35899b1627b53e6
Nick McCullough <91184284+mccnick@users.noreply.github.com>
2023-03-26 22:57:41 -0500

    Add files via upload

---
+++ Problem2.m
@@ -0,0 +1,6 @@
+% Nick McCullough, AerE 161, Project 1, Problem 2 function
+
+function tempC = Problem2(temp) % create function "ktoc" Kelvin to Celsius.
+
+tempC = temp - 273.15; % conversion equation for given, Kelvin, to Celsius.
+end % end function
